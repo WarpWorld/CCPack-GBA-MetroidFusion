ffdc4d62ce40c0418fd190889423d4bd38b0cf39
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2025-10-03 19:01:20 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- MetroidFusion.cs
+++ MetroidFusion.cs
@@ -25,30 +25,31 @@ partial class MetroidFusion : GBAEffectPack, IHandlerCollection
 
     public override EffectList Effects { get; } = new Effect[]
     {
-        new("Kill Samus", "killplayer"),
-        new("Force Morphball", "forceMorph") { Duration = 15 },
-        new("Freeze Samus", "freeze") { Duration = 10 },
+        new("Kill Samus", "killplayer") { Alignment = (Alignment)Morality.ExtremelyHarmful + Orderliness.Controlled },
+        new("Force Morphball", "forceMorph") { Duration = 15, Alignment = (Alignment)Morality.SlightlyHarmful + Orderliness.Controlled /* TODO: Confirm severity (mobility tradeoffs) */ },
+        new("Freeze Samus", "freeze") { Duration = 10, Alignment = (Alignment)Morality.Harmful + Orderliness.Controlled },
 
-        new("Enable Shinespark", "shinespark"),
+        new("Enable Shinespark", "shinespark") { Alignment = (Alignment)Morality.Helpful + Orderliness.Controlled },
         //new("Force Shinespark", "shinesparkForced"),
-        new("Slow Samus", "slow") { Duration = 15 },
+        new("Slow Samus", "slow") { Duration = 15, Alignment = (Alignment)Morality.SlightlyHarmful + Orderliness.Controlled },
 
-        new("Open Map", "openMap"),
-        new("SA-X Close Up!", "saxCutscene"),
-        new("Restart Game", "reboot"),
+        new("Open Map", "openMap") { Alignment = (Alignment)Morality.SlightlyHelpful + Orderliness.Controlled },
+        new("SA-X Close Up!", "saxCutscene") { Alignment = (Alignment)Morality.SlightlyHarmful + Orderliness.Chaotic }, /* not actually harmful, but disruptive */
+        new("Restart Game", "reboot") { Alignment = (Alignment)Morality.ExtremelyHarmful + Orderliness.Controlled },
 
-        new("Give Missiles", "missiles_add") { Quantity = 999 },
-        new("Take Missiles", "missiles_remove") { Quantity = 999 },
-        new("Give Power Bombs", "bombs_add") { Quantity = 99 },
-        new("Take Power Bombs", "bombs_remove") { Quantity = 99 },
+        new("Give Missiles", "missiles_add") { Quantity = 999, Alignment = (Alignment)Morality.Helpful + Orderliness.Controlled },
+        new("Take Missiles", "missiles_remove") { Quantity = 999, Alignment = (Alignment)Morality.VeryHarmful + Orderliness.Controlled },
+        new("Give Power Bombs", "bombs_add") { Quantity = 99, Alignment = (Alignment)Morality.Helpful + Orderliness.Controlled },
+        new("Take Power Bombs", "bombs_remove") { Quantity = 99, Alignment = (Alignment)Morality.VeryHarmful + Orderliness.Controlled },
 
-        new("Fire Charge Beam", "fire_chargebeam"),
-        new("Fire Missiles", "fire_missile"),
-        new("Fire Bombs", "fire_bomb"),
-        new("Fire Charge Bombs", "fire_chargebomb"),
-        new("Fire Power Bombs", "fire_powerbomb"),
+        /* Mixed: can consume resources but can help offensively or with obstacles */
+        new("Fire Charge Beam", "fire_chargebeam") { Alignment = (Alignment)Morality.Neutral + Orderliness.SlightlyChaotic },
+        new("Fire Missiles", "fire_missile") { Alignment = (Alignment)Morality.Neutral + Orderliness.SlightlyChaotic },
+        new("Fire Bombs", "fire_bomb") { Alignment = (Alignment)Morality.Neutral + Orderliness.SlightlyChaotic },
+        new("Fire Charge Bombs", "fire_chargebomb") { Alignment = (Alignment)Morality.Neutral + Orderliness.SlightlyChaotic },
+        new("Fire Power Bombs", "fire_powerbomb") { Alignment = (Alignment)Morality.Neutral + Orderliness.SlightlyChaotic },
 
-        new("One-Hit KO", "ohko") { Duration = 15 },
+        new("One-Hit KO", "ohko") { Duration = 15, Alignment = (Alignment)Morality.VeryHarmful + Orderliness.Controlled },
 
         //language_jp", "language_en", "language_de", "language_fr", "language_it", "language_es
         /*new("Game Language", "language", ItemKind.BidWar)
